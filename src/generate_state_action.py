5c8ed80bbc268b5d62bd7f9c84e87323cb7dacd7
Sudarshan <sudarshan85@gmail.com>
2015-12-15 08:14:30 -0500

    Added function to generate valid actions for given state. Not completed yet.

--- src/generate_state_action.py
+++ src/generate_state_action.py
@@ -5,9 +5,18 @@ from pprint import pprint
 from random import choice
 from itertools import product
 
+actions = {
+           'WG': 'Wait for teammate to receive',
+           'WS': 'Wait for state change',
+           'T': 'Take box from table',
+           'G': 'Give box to teammate',
+           'K': 'Keep box on table',
+           'R': 'Receive box from teammate',
+           'X': 'Exit task'
+           }
+
 MAX_BOXES = 8
 MAX_BOXES_ACC = MAX_BOXES/2
-
 State = namedtuple("State", ['n_r', 'n_h', 't_r', 't_h', 'b_r', 'b_h', 'e'])
 
 def get_start_states():
@@ -35,8 +44,14 @@ def is_valid_state(state):
         return False
     return True
 
+def get_valid_actions(state):
+    actions = list()
+    if all(v == 0 for v in state):
+        actions.append('WS')
+    return actions
+
 def generate_states():
-    states = list()
+    states = set()
     state_vals = [
                     [v for v in range(MAX_BOXES_ACC+1)],
                     [v for v in range(MAX_BOXES_ACC+1)],
@@ -46,18 +61,22 @@ def generate_states():
                     [0, 1],
                     [0, 1]
                  ]
-    for state in product(*state_vals):
-        if is_valid_state(State(*state)):
-            states.append(state)
+    for s in product(*state_vals):
+        state = State(*s)
+        if is_valid_state(state):
+            states.add(state)
 
     return states
 
 if __name__=='__main__':
-    #start_states = get_start_states()
+    start_states = get_start_states()
     #state = choice(tuple(start_states))
     #state = State(0, 0, 0, 0, 0, 0, 0)
     #print is_valid_state(state)
     #print state
     states = generate_states()
-    print len(states)
+    state_action = dict([ (elem, None) for elem in states ])
+    for state in state_action:
+        state_action[state] = get_valid_actions(state)
+    pprint(state_action, width=1)
 
