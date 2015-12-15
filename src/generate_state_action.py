72643cc5069fe8482357c544266833d5f7dccdaf
Sudarshan <sudarshan85@gmail.com>
2015-12-15 08:49:21 -0500

    Added comments

--- src/generate_state_action.py
+++ src/generate_state_action.py
@@ -5,53 +5,37 @@ from pprint import pprint
 from random import choice
 from itertools import product
 
-actions = {
-           'WG': 'Wait for teammate to receive',
-           'WS': 'Wait for state change',
-           'T': 'Take box from table',
-           'G': 'Give box to teammate',
-           'K': 'Keep box on table',
-           'R': 'Receive box from teammate',
-           'X': 'Exit task'
-           }
+possible_actions = {
+                        'WG': 'Wait for teammate to receive',
+                        'WS': 'Wait for state change',
+                        'T': 'Take box from table',
+                        'G': 'Give box to teammate',
+                        'K': 'Keep box on table',
+                        'R': 'Receive box from teammate',
+                        'X': 'Exit task'
+                   }
 
 MAX_BOXES = 8
 MAX_BOXES_ACC = MAX_BOXES/2
-State = namedtuple("State", ['n_r', 'n_h', 't_r', 't_h', 'b_r', 'b_h', 'e'])
+State = namedtuple("State",
+                        [   'n_r', # number of robot's boxes on its side 0..MAX_BOXES_ACC
+                            'n_h', # number of teammate's boxes on its side 0..MAX_BOXES_ACC
+                            't_r', # Is the robot transferring? Y/N
+                            't_h', # Is the teammate transferring? Y/N
+                            'b_r', # Is the robot holding its box? Y/N
+                            'b_h', # Is the robot holding the teammate's box? Y/N
+                            'e'    # Is the task completed? Y/N
+                        ]
+                  )
 
 def get_start_states():
     start_states = set()
     for i in range(MAX_BOXES_ACC+1):
-        state = State(n_r = i, n_h = MAX_BOXES_ACC - i, t_r = 0, t_h = 0, b_r = 0, b_h = 0, e = 0)
-        start_states.add(state)
+        start_states.add(State(i, MAX_BOXES_ACC-i, 0, 0, 0, 0, 0))
     return start_states
 
-def is_valid_state(state):
-    if state.e == 1:
-        # task is done, so other elements of the state vector cannot be non-zero
-        return all(v == 0 for v in state[0:-1])
-    if state.n_r + state.n_h > MAX_BOXES_ACC:
-        # total number of boxes on robot's side must be less than or equal to max number of books
-        # or zero when robot's side is sorted and robot is waiting for human
-        return False
-    if state.t_r == 1 and state.b_h != 1:
-        # if the robot is transferring a box, then it must be holding the
-        # human's box for the state to be valid
-        return False
-    if state.t_h == 1 and state.b_r == 1 and state.b_h == 1:
-        # if robot is holding human's box and human is transferring, the
-        # robot will not be holding its own box as well
-        return False
-    return True
-
-def get_valid_actions(state):
-    actions = list()
-    if all(v == 0 for v in state):
-        actions.append('WS')
-    return actions
-
 def generate_states():
-    states = set()
+    states = list()
     state_vals = [
                     [v for v in range(MAX_BOXES_ACC+1)],
                     [v for v in range(MAX_BOXES_ACC+1)],
@@ -64,9 +48,35 @@ def generate_states():
     for s in product(*state_vals):
         state = State(*s)
         if is_valid_state(state):
-            states.add(state)
+            states.append(state)
 
-    return states
+    return frozenset(states)
+
+def is_valid_state(state):
+    if state.e == 1:
+        # task is done, so other elements of the state vector cannot be non-zero
+        return all(v == 0 for v in state[0:-1])
+    if state.n_r + state.n_h > MAX_BOXES_ACC:
+        # total number of boxes on robot's side must be less than or equal to max number of books
+        # or zero when robot's side is sorted and robot is waiting for teammate
+        return False
+    if state.t_r == 1 and state.b_h != 1:
+        # if the robot is transferring a box, then it must be holding the
+        # teammate's box for the state to be valid
+        return False
+    if state.t_h == 1 and state.b_r == 1 and state.b_h == 1:
+        # if robot is holding teammate's box and teammate is transferring, the
+        # robot will not be holding its own box as well
+        return False
+    return True
+
+def get_valid_actions(state):
+    actions = list()
+    if all(v == 0 for v in state):
+        # robot is done with its part and can only wait for teammate to change
+        # state
+        actions.append('WS')
+    return actions
 
 if __name__=='__main__':
     start_states = get_start_states()
@@ -78,5 +88,5 @@ if __name__=='__main__':
     state_action = dict([ (elem, None) for elem in states ])
     for state in state_action:
         state_action[state] = get_valid_actions(state)
-    pprint(state_action, width=1)
+    pprint(state_action)
 
