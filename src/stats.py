37236f6e0d63e73ae44cb46b539836d3a97ee470
Sudarshan <sudarshan85@gmail.com>
2016-01-02 07:34:59 -0500

    Added command line arg for ntrials

--- src/stats.py
+++ src/stats.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python
 
+import sys
 import random
 import logging
 import numpy as np
@@ -13,7 +14,7 @@ if __name__=='__main__':
     logging.basicConfig(level=logging.ERROR, format='%(asctime)s-%(levelname)s: %(message)s')
     task_states, task_start_states, task_state_action_map = ts.load_states()
     expert_visited_states, expert_state_action_map, time_per_step = commonHumanPolicy.read_data(task_states, task_start_states)
-    ntrials = 10000
+    ntrials = int(sys.argv[1]) if len(sys.argv) > 1 else 100
     nactions_expert = np.zeros(ntrials)
     nactions_random = np.zeros(ntrials)
     for i in range(ntrials):
@@ -28,3 +29,13 @@ if __name__=='__main__':
     print "Mean:\t\t ",format(np.mean(nactions_expert), '.3f'), "\t\t\t", format(np.mean(nactions_random), '.3f')
     print "Var:\t\t ", format(np.var(nactions_expert), '.3f'), "\t\t\t", format(np.var(nactions_random), '.3f')
     print "Std:\t\t ", format(np.std(nactions_expert), '.3f'), "\t\t\t", format(np.std(nactions_random), '.3f')
+
+#Number of trials =  100000
+#Metric: Number of action per trial
+#********************************************************************************
+                #Expert Policy               Random Policy
+#********************************************************************************
+#Mean:             16.233                        17.282
+#Var:              19.873                        33.267
+#Std:              4.458                         5.768
+
